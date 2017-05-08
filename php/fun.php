c8b9237fc50fb7efa95593d56d8a251d83a51bd9
Pablo <Pablo@Pablo-PC>
2017-05-08 12:45:58 +0200

    primer proyecto de ejemplo con git implementando hello world

--- php/fun.php
+++ php/fun.php
@@ -26,6 +26,14 @@ function TodosConciertos(){
     return $query;
     
 }
+function TodosMusicos(){
+    
+    $con = conectar();
+    $query = mysqli_query($con, 'select * from musico');
+    
+    return $query;
+    
+}
 function getMejorConcierto(){
     
     $con = conectar();
@@ -40,9 +48,43 @@ order by valora.val desc limit 1');
     
 }
 function getConcierto(){
-    
     $con = conectar();
     $query = mysqli_query($con, 'select concierto.nombre, concierto.fecha from concierto order by fecha asc');
     return $query;
+}
+
+function votar($id_concierto, $user){
+    $con = conectar();
+    $query=  mysqli_query($con, "insert into valora(fan_username, id_concierto, val) values ('".$user."','".$id_concierto."',1)");
+    return $query;
+    
+}
+function votarmusico($musico_username){
+    $con = conectar();
+    $query=  mysqli_query($con, "insert into musico (musico_username, val) values ('".$musico_username."',1)");
+    return $query;
+    
+}
+function suma($id_concierto){
+    $con = conectar();
+    $query=  mysqli_query($con, "select sum(val) as sum from valora where id_concierto='$id_concierto'");
+    return $query;
+}
+function sumamusico($musico_username){
+    $con = conectar();
+    $query=  mysqli_query($con, "select sum(val) as sum from musico where musico_username='$musico_username'");
+    return $query;
+}
+function revisar($id_concierto, $user){
+    $con = conectar();
+    $query=  "SELECT * FROM valora where fan_username = '$user' and id_concierto = '$id_concierto'";
+    $resultado = mysqli_query($con, $query);
+    $filas = mysqli_num_rows($resultado);
+    
+    if ($filas > 0) {
+        return true;
+    } else {    // Este else no hace falta
+        return false;
+    }
 }
 ?>
--- votar_musicos.php
+++ votar_musicos.php
@@ -11,7 +11,7 @@ and open the template in the editor.
         <title>VOTAR CONCIERTO</title>
     </head>
     <body>
-        <div id="votar1"><h1>Vota el concierto si te ha gustado</h1>
+        <div id="votar1"><h1>Vota el musico si te ha gustado</h1>
             <?php
             require_once 'php/fun.php';
             session_start();
@@ -21,7 +21,7 @@ and open the template in the editor.
             require_once 'php/fun.php';
             echo "<form action='' method='POST'>";
 
-            $query = TodosConciertos();
+            $query = TodosMusicos();
             $query2 = array_shift(mysqli_fetch_array($query));
             ?>
             </div>
@@ -33,28 +33,28 @@ require_once 'php/fun.php';
 if (isset($_POST["boton1"])) {
     // $user = $_SESSION["user"];
     $user = "hola2";
-    $id_concierto = $_POST["id_concierto"];
-    if (revisar($id_concierto, $user)==false) {
-        votar($id_concierto, $user);
+    $musico_username = $_POST["musico_username"];
+    if (revisar($musico_username, $user)==false) {
+        votar($musico_username);
         
     } 
 }
 
 
 
-$query = TodosConciertos();
+$query = TodosMusicos();
 
 $query2 = array_shift(mysqli_fetch_array($query));
 echo '<table>';
-echo '<tr><td>' . 'Nombre' . '</td><td>' . 'fecha' . '</td><td>' . 'desc' . '</td><td>' . 'id_estilo' . '</td><td>' . 'id_ciudad' . '</td><td>' . 'votar' . '</td><td>' . 'puntos' . '</td></tr>';
+echo '<tr><td>' . 'nombrec' . '</td><td>' . 'nombreg' . '</td><td>' . 'nombrea' . '</td><td>' . 'web' . '</td><td>' . 'telef' . '</td><td>' . 'votar' . '</td><td>' . 'puntos' . '</td></tr>';
 echo '</table>';
 foreach ($query as $key => $value) {
     extract($value);
-    $suma = suma($id_concierto);
+    $suma = sumamusico($musico_username);
     extract(mysqli_fetch_array($suma));
     echo '<form action="" method="POST">';
     echo '<table>';
-    echo '<tr><td>' . $nombre . '<input name="id_concierto" value=' . $id_concierto . '" style="display:none"></td><td>' . $fecha . '</td><td>' . $desc . '</td><td>' . $id_estilo . '</td><td>' . $id_ciudad . '</td><td>' . '<input type="submit"  name="boton1" class="button" value="VOTAR">' . '</td><td>' . $sum . '</td></tr></form>';
+    echo '<tr><td>' . $nombrec . '<input name="musico_username" value=' . $musico_username . '" style="display:none"></td><td>' . $nombreg . '</td><td>' . $nombrea . '</td><td>' . $web . '</td><td>' . $telef . '</td><td>' . '<input type="submit"  name="boton1" class="button" value="VOTAR">' . '</td><td>' . $sum . '</td></tr></form>';
     echo '</table>';
 }
 ?>
